6075dfa1513af9d12e39774e801aea9f07d7ccc7
Malcolm Smith <smith@chaquo.com>
2024-03-09 13:45:59 +0000

    Raise an error when building on non-Linux platforms

--- build.sh
+++ build.sh
@@ -6,6 +6,15 @@ version=${2:?}
 build=${3:?}
 export HOST=${4:?}
 
+if [ "$(uname)" != "Linux" ]; then
+    echo 'These scripts may not work on non-Linux platforms. For example, building '
+    echo 'OpenSSL 3.0.13 on macOS led to the runtime error: dlopen failed: cannot find '
+    echo '"libcrypto_python.so" from verneed[1] in DT_NEEDED list for '
+    echo '"/data/data/org.python.testbed/files/python/lib/python3.13/lib-dynload/_ssl.cpython-313-aarch64-linux-android.so".'
+    echo 'The cause of this is unknown.'
+    exit 1
+fi
+
 recipe_dir=$(dirname $(realpath $0))/$name
 $recipe_dir/build.sh $version
 
