d19274858563fa66b5792b2ddfdf602d9f91b3dc
Malcolm Smith <smith@chaquo.com>
2025-02-10 19:34:50 +0000

    Update to SQLite 3.49.0

--- sqlite/build.sh
+++ sqlite/build.sh
@@ -5,7 +5,6 @@ recipe_dir=$(dirname $(realpath $0))
 
 # Unfortunately the URL represents the version as an integer, and also includes a year
 # subdirectory. For example, version 3.39.2 is at /2022/sqlite-autoconf-3390200.tar.gz.
-year=2024
 version=${1:?}
 read major minor micro < <(echo $version | tr '.' ' ')
 version_int=$(printf "%d%02d%02d00" $major $minor $micro)
@@ -39,10 +38,9 @@ tar -xf $version_dir/$src_filename
 cd $(basename $src_filename .tar.gz)
 
 patch -p1 -i "$recipe_dir/soname.patch"
-patch -p1 -i "$recipe_dir/strerror_r.patch"
 
 CFLAGS+=" -Os"  # This is off by default, but it's recommended in the README.
-./configure --host=$HOST --disable-static --disable-static-shell --with-pic
+./configure --host=$HOST --disable-static
 make -j $CPU_COUNT
 
 prefix=$build_dir/prefix
