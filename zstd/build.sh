fea63997d13018ebe68de476a4d3f4facff3b421
Emma Smith <emma@emmatyping.dev>
2025-07-02 12:27:39 -0700

    Add zstd build script (#5)

---
+++ zstd/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eu
+
+recipe_dir=$(dirname $(realpath $0))
+version=${1:?}
+
+cd $recipe_dir
+
+# zstd was added to Python in version 3.14, which uses this minimum API level.
+# zstd also requires the same API level for fseeko and ftello on 32-bit ABIs
+# (https://android.googlesource.com/platform/bionic/+/HEAD/docs/32-bit-abi.md).
+ANDROID_API_LEVEL=24
+. ../android-env.sh
+
+version_dir=$recipe_dir/build/$version
+mkdir -p $version_dir
+cd $version_dir
+src_filename=zstd-$version.tar.gz
+wget -c https://github.com/facebook/zstd/releases/download/v$version/$src_filename
+
+build_dir=$version_dir/$HOST
+rm -rf $build_dir
+mkdir $build_dir
+cd $build_dir
+tar -xf $version_dir/$src_filename
+cd zstd-$version
+
+prefix=$build_dir/prefix
+mkdir $prefix
+
+make -j $CPU_COUNT lib-release PREFIX=$prefix
+make install PREFIX=$prefix
